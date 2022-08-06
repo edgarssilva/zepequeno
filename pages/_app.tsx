92f87893d9e5389db80124bb6109cccfd32f559f
Edgar Silva <edgarasdaa10@gmail.com>
2022-08-06 19:54:49 +0100

    Simple Navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
-import { MantineProvider } from "@mantine/core";
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+} from "@mantine/core";
+import { getCookie, setCookie } from "cookies-next";
 import { AppProps } from "next/app";
 import Head from "next/head";
+import { GetServerSidePropsContext } from "next/types";
+import { useState } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App(props: AppProps & { colorScheme: ColorScheme }) {
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(
+    props.colorScheme
+  );
+
+  const toggleColorScheme = (value?: ColorScheme) => {
+    const nextColorScheme =
+      value || (colorScheme === "dark" ? "light" : "dark");
+    setColorScheme(nextColorScheme);
+    setCookie("mantine-color-scheme", nextColorScheme, {
+      maxAge: 60 * 60 * 24 * 30,
+    });
+  };
+
   return (
     <>
       <Head>
@@ -11,16 +31,24 @@ export default function App({ Component, pageProps }: AppProps) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
+        <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          colorScheme: "light",
-        }}
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
       >
-        <Component {...pageProps} />
-      </MantineProvider>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{ colorScheme }}
+        >
+          <props.Component {...props.pageProps} />
+        </MantineProvider>
+      </ColorSchemeProvider>
     </>
   );
 }
+
+App.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
+  colorScheme: getCookie("mantine-color-scheme", ctx) || "light",
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
 import { Title } from "@mantine/core";
 import type { NextPage } from "next";
+import Navbar from "../components/Navbar";
 
 const Home: NextPage = () => {
-  return <Title>Hi</Title>;
+  return (
+    <>
+      <Navbar
+        links={[
+          { label: "Home", link: "#" },
+          { label: "Contactos", link: "#contact" },
+        ]}
+      />
+      <Title>Hi</Title>
+    </>
+  );
 };
 
 export default Home;
